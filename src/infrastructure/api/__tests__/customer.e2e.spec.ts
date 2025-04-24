9343a1d38bec80335efa47d730214b73fb33c753
Joao Pedro Araujo <joao.araujo@arancia.ca>
2025-04-23 22:45:31 -0300

    feat(customer): added customer creation endpoint and e2e test
    - added express server setup with SQLite in-memory

---
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,33 @@
+import { app, sequelize } from '../express';
+import request from 'supertest';
+
+describe("e2e test for customer", () => {
+    beforeEach(async () => {
+        await sequelize.sync({ force: true })
+    });
+
+    afterAll(async () => {
+        await sequelize.close();
+    });
+
+    it("should create a costumer", async () => {
+        const response = await request(app)
+            .post("/customers")
+            .send({
+                name: "John",
+                address: {
+                    street: "Street",
+                    city: "City",
+                    number: 123,
+                    zip: "12345"
+                }
+            });
+
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe("John");
+        expect(response.body.address.street).toBe("Street");
+        expect(response.body.address.city).toBe("City");
+        expect(response.body.address.number).toBe(123);
+        expect(response.body.address.zip).toBe("12345");
+    });
+});
---
+++ src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+import CreateCustomerUseCase from '../../../usecase/customer/create/create.customer.usecase';
+import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
+import e from 'express';
+
+export const customerRoute = express.Router();
+
+customerRoute.post("/", async (req: Request, res: Response) => {
+    const useCase = new CreateCustomerUseCase(new CustomerRepository());
+    try {
+        const customerDTO = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                city: req.body.address.city,
+                number: req.body.address.number,
+                zip: req.body.address.zip,
+            },
+        }
+        const output = await useCase.execute(customerDTO)
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
